54362b67a0b7d391592a0583a6ccb1587a8f4de3
Quentin <quentinsims89@gmail.com>
2021-07-24 14:46:44 +0200

    Initial commit

---
+++ CovidResults.cs
@@ -0,0 +1,93 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+
+namespace CovidStats
+{
+    public class CovidResults
+    {
+        [Name("iso_code")]
+        public string isoCode { get; set; }
+
+        [Name("continent")]
+        public string Continent { get; set; }
+
+        [Name("location")]
+        public string Location { get; set; }
+
+        [Name("date")]
+        public DateTime Date { get; set; }
+
+        [Name("popluation")]
+        public decimal Popluation { get; set; }
+
+        [Name("total_cases")]
+        public decimal TotalCases { get; set; }
+
+        [Name("new_cases")]
+        public decimal NewCases { get; set; }
+
+        [Name("new_cases_smoothed")]
+        public decimal NewCasesSmoothed { get; set; }
+
+        [Name("total_deaths")]
+        public decimal TotalDeaths { get; set; }
+
+        [Name("new_deaths")]
+        public decimal NewDeaths { get; set; }
+
+        [Name("new_deaths_smoothed")]
+        public decimal NewDeathsSmoothed { get; set; }
+
+        [Name("total_cases_per_million")]
+        public decimal TotalCasesPerMillion { get; set; }
+
+        [Name("new_cases_per_million")]
+        public decimal NewCasesPerMillion { get; set; }
+
+        [Name("new_cases_smoothed_per_million")]
+        public decimal NewCasesSmoothedPerMillion { get; set; }
+
+        [Name("Total_deaths_per_million")]
+        public decimal TotalDeathsPerMillion { get; set; }
+
+        [Name("new_deaths_per_million")]
+        public decimal NewDeathsPerMillion { get; set; }
+
+        [Name("new_deaths_smoothed_per_million")]
+        public decimal NewDeathSmoothedPerMillion { get; set; }
+
+        [Name("reproduction_rate")]
+        public decimal ReproductionRate { get; set; }
+
+        [Name("icu_patients")]
+        public decimal IcuPatients { get; set; }
+
+        [Name("icu_patients_per_million")]
+        public decimal IcuPatientsPerMillion { get; set; }
+
+        [Name("hosp_patients")]
+        public decimal HospPatients { get; set; }
+
+        [Name("hosp_patients_per_million")]
+        public decimal HospPatientsPerMillion { get; set; }
+
+        [Name("weekly_icu_admissions")]
+        public decimal WeeklyIcuAdmissions { get; set; }
+
+        [Name("weekly_icu_admissions_per_million")]
+        public decimal WeeklyIcuAdmissionsPerMillion { get; set; }
+
+        [Name("weekly_hosp_admissions")]
+        public decimal WeeklyHospAdmissions { get; set; }
+
+        [Name("weekly_hosp_admissions_per_million")]
+        public decimal WeeklyHospAdmissionsPerMillion { get; set; }
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CovidStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo covidDirectory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(covidDirectory.FullName, "covid deaths.csv");
+            var fileContents = ReadCovidResults(fileName);
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static List<CovidResults> ReadCovidResults(string fileName)
+        {
+            var covidData = new List<CovidResults>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                reader.ReadLine();
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var covidResults = new CovidResults();
+                    string[] values = line.Split(';');
+
+                    covidResults.isoCode = values[0];
+                    covidResults.Continent = values[1];
+                    covidResults.Location = values[2];
+
+                    covidResults.Date = DateTime.Parse(values[3]);
+                    DateTime date;
+                    if (DateTime.TryParse(values[3], out date))
+                    {
+                        covidResults.Date = date;
+                    }
+                    decimal parseDecimal;
+                    if (decimal.TryParse(values[4].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.Popluation = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[5].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.TotalCases = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[6].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.NewCases = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[7].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.NewCasesSmoothed = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[8].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.TotalDeaths = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[9].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.NewDeaths = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[10].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.NewDeathsSmoothed = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[11].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.TotalCasesPerMillion = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[12].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.NewCasesPerMillion = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[13].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.NewCasesSmoothedPerMillion = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[14].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.TotalDeathsPerMillion = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[15].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.NewDeathsPerMillion = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[16].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.NewDeathSmoothedPerMillion = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[17].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.ReproductionRate = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[18].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.IcuPatients = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[19].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.IcuPatientsPerMillion = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[20].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.HospPatients = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[21].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.HospPatientsPerMillion = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[22].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.WeeklyIcuAdmissions = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[23].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.WeeklyHospAdmissionsPerMillion = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[24].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.WeeklyHospAdmissions = parseDecimal;
+                    }
+                    if (decimal.TryParse(values[25].Replace(".", ","), out parseDecimal))
+                    {
+                        covidResults.WeeklyHospAdmissionsPerMillion = parseDecimal;
+                    }
+
+                    covidData.Add(covidResults);
+                }
+            }
+            return covidData;
+        }
+    }
+}
